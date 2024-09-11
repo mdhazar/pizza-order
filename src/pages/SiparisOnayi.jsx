ba2d6855a229dee2d004d1923fee5124520c3b47
mdhazar <m.derya.hazar@gmail.com>
2024-09-11 17:40:06 +0300

    Added padding and margin

--- src/pages/SiparisOnayi.jsx
+++ src/pages/SiparisOnayi.jsx
@@ -2,34 +2,39 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ConfirmationContainer = styled.div`
-  
-  margin: 50px auto;
-  background-color:  #CE2829;
-  
-  padding: 200px;
-
+  position: relative; 
+  padding: 100px;
+  background-color: #CE2829;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0;
 `;
 
 const ConfirmationTitle = styled.h2`
   font-size: 2em;
   color: white;
+  font-weight: bold;
+  gap: 0;
+  margin-bottom: 300px;
 `;
 
 const ConfirmationMessage = styled.p`
-  font-size: 1.2em;
+  font-size: 2em;
   color: white;
+  text-align: center;
+  font-weight: 100;
+  gap: 0;
+  margin: 0; 
 `;
 
 function SiparisOnayi() {
   return (
     <ConfirmationContainer>
       <ConfirmationTitle>Teknolojik Yemekler</ConfirmationTitle>
-      <ConfirmationMessage>
-        Tebrikler!
-      </ConfirmationMessage>
-      <ConfirmationMessage>
-        SİPARİŞİNİZ ALINDI!
-      </ConfirmationMessage>
+      <ConfirmationMessage>Tebrikler!</ConfirmationMessage>
+      <ConfirmationMessage>SİPARİŞİNİZ ALINDI!</ConfirmationMessage>
     </ConfirmationContainer>
   );
 }
